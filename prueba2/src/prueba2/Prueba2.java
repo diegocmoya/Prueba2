2b4c90ab07dd04f1fadb47c5cf86be558be9d594
Diego <Diego@Garro>
2015-04-07 11:16:35 -0600

    otra subida archivos

--- prueba2/src/prueba2/Prueba2.java
+++ prueba2/src/prueba2/Prueba2.java
@@ -16,7 +16,9 @@ public class Prueba2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("hola");
+        
+        // Este es el Cambio De Diego Garro
+        System.out.print("Hola mundo");
     }
     
 }
